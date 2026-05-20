661f1235d3e850470bb068b4da88d32d96051153
Dan Caseley <dan@mobile.dev>
2026-05-20 15:49:31 +0100

    Defend against unusual formatting in bounds values

--- maestro-client/src/test/java/maestro/drivers/WebDriverTest.kt
+++ maestro-client/src/test/java/maestro/drivers/WebDriverTest.kt
@@ -1,5 +1,6 @@
 package maestro.drivers
 
+import com.google.common.truth.Truth.assertThat
 import io.mockk.every
 import io.mockk.mockk
 import maestro.web.selenium.SeleniumFactory
@@ -9,6 +10,18 @@ import java.util.ServiceConfigurationError
 import org.openqa.selenium.WebDriver as SeleniumWebDriver
 
 class WebDriverTest {
+
+    private fun makeDriver(): WebDriver {
+        val seleniumDriver = mockk<SeleniumWebDriver>(relaxed = true)
+        return WebDriver(
+            isStudio = false,
+            isHeadless = true,
+            screenSize = null,
+            seleniumFactory = object : SeleniumFactory {
+                override fun create(): SeleniumWebDriver = seleniumDriver
+            },
+        )
+    }
 
     @Test
     fun `open() does not propagate ServiceConfigurationError from DevTools setup`() {
@@ -34,5 +47,38 @@ class WebDriverTest {
         )
 
         driver.open()
+    }
+
+    @Test
+    fun `parseDomAsTreeNodes handles bounds as String`() {
+        val dom = mapOf(
+            "attributes" to mapOf("text" to "Button", "bounds" to "[10,20][110,60]"),
+            "children" to emptyList<Any>(),
+        )
+        val node = makeDriver().parseDomAsTreeNodes(dom)
+        assertThat(node.attributes["bounds"]).isEqualTo("[10,20][110,60]")
+    }
+
+    @Test
+    fun `parseDomAsTreeNodes handles bounds as LinkedHashMap`() {
+        val dom = mapOf(
+            "attributes" to mapOf(
+                "text" to "Button",
+                "bounds" to mapOf("left" to 10, "top" to 20, "right" to 110, "bottom" to 60),
+            ),
+            "children" to emptyList<Any>(),
+        )
+        val node = makeDriver().parseDomAsTreeNodes(dom)
+        assertThat(node.attributes["bounds"]).isEqualTo("[10,20][110,60]")
+    }
+
+    @Test
+    fun `parseDomAsTreeNodes uses fallback for unknown bounds type`() {
+        val dom = mapOf(
+            "attributes" to mapOf("text" to "Button", "bounds" to 42L),
+            "children" to emptyList<Any>(),
+        )
+        val node = makeDriver().parseDomAsTreeNodes(dom)
+        assertThat(node.attributes["bounds"]).isEqualTo("[0,0][0,0]")
     }
 }
